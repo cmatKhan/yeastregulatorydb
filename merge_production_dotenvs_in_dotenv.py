b079f26328a2a5f5ffcc15a50df6875d6094ce39
chase mateusiak <chase.mateusiak@gmail.com>
2024-01-23 13:53:19 -0600

    adding regulatory data envs to production dotenv.py script

--- merge_production_dotenvs_in_dotenv.py
+++ merge_production_dotenvs_in_dotenv.py
@@ -7,6 +7,7 @@ PRODUCTION_DOTENVS_DIR = BASE_DIR / ".envs" / ".production"
 PRODUCTION_DOTENV_FILES = [
     PRODUCTION_DOTENVS_DIR / ".django",
     PRODUCTION_DOTENVS_DIR / ".postgres",
+    PRODUCTION_DOTENVS_DIR / ".regulatory_data",
 ]
 DOTENV_FILE = BASE_DIR / ".env"
 
