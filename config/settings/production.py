a5aca23a8b948be098d7f13bd6be2bad28f54939
chase mateusiak <chase.mateusiak@gmail.com>
2024-02-26 06:42:16 -0600

    debugging new env redis strings in config

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
-REDIS_URL = "redis://" + env("REDIS_HOST", "redis") + ":" + env("REDIS_PORT", "6379") + "/0"
+REDIS_URL = "redis://" + env.str("REDIS_HOST", "redis") + ":" + env.str("REDIS_PORT", "6379") + "/0"
 
 # DATABASES
 # ------------------------------------------------------------------------------
